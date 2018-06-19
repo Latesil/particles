d4c36b223c6d7ea8f378c0bb43c3d9192f988ab0
Latesil <skipastas@gmail.com>
2018-06-19 21:30:24 +0300

    spiral

--- particle.cpp
+++ particle.cpp
@@ -5,14 +5,18 @@
 namespace sdlone {
 Particle::Particle(): m_x(0), m_y(0) {
     m_direction = (2 * M_PI * rand())/RAND_MAX;
-    m_speed = (0.0001 * rand())/RAND_MAX;
+    m_speed = (0.04 * rand())/RAND_MAX;
+
+    m_speed *= m_speed;
 }
 
 Particle::~Particle() {
-    // TODO Auto-generated destructor stub
 }
 
 void Particle::update(bool explosion, int interval) {
+
+    m_direction += interval * 0.0004;
+
     double xspeed = m_speed * cos(m_direction);
     double yspeed = m_speed * sin(m_direction);
 
